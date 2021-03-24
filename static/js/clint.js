e23f65a882b9247525aff903e9e3f03fb2155aba
Khunt Yashik <yashikkhunt2002@gmail.com>
2021-03-24 09:21:05 +0530

    Some Changes

--- static/js/clint.js
+++ static/js/clint.js
@@ -1,4 +1,34 @@
+//Selectors
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+var header = document.getElementById('navbar');
+var main_back = document.querySelector('.background');
+
+// Animate The navigation bar when it scroll down in Y axis
 window.addEventListener("scroll",function(){
-    var header = document.getElementById('navbar');
-    header.classList.toggle("sticky",window.scrollY > 75);
+  header.classList.toggle("sticky",window.scrollY > 30);
+  main_back.style.opacity="0.8"
+  if(window.scrollY < 100){
+    main_back.style.opacity = "1";
+  }
 });
+
+//function
+
+// animated text testing
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 1500,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 1600,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
